e08d12b90046e27f2027a7d9fbebb4245f669e2d
Jack <jdcorley@gmail.com>
2017-09-28 16:07:40 -0400

    Assignment 14 completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -24,10 +24,14 @@ $( document ) .ready(function() {
   $('#time-control input').on('input', function (event){
     player.skipTo(event.target.value);
   });
+  $('#volume-control input').on('input', function (event){
+    player.setVolume(event.target.value);
+  });
   setInterval ( () => {
     const currentTime = player.getTime();
     const duration = player.getDuration();
     const percent = (currentTime / duration) * 100;
+    $('#time-control .total-time').text(duration);
     $('#time-control .current-time').text( currentTime );
     $('#time-control input').val(percent);
   }, 1000);
